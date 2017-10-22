d34a9b8d14d48a9ec7768d501545078b41d778d5
bissanmu <bissanmu@gmail.com>
2017-10-22 14:34:08 +0900

    Initial Commit

---
+++ StandardTerm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StandardTerm
+//
+//  Created by admin on 2017. 10. 22..
+//  Copyright © 2017년 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
